84ae33a741f289ce62cd67cbf59e64cd0725e13c
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-07-08 09:27:48 +0200

    Add skeleton for user auth pages

---
+++ client/src/pages/signup/signup.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//style
+import './signup.style.scss';
+
+const SignUp = () => {
+    return(
+        <div className="signup-container">
+            Signup
+        </div>
+    )
+}
+
+export default SignUp;
