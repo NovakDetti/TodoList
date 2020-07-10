2511ad58823930804ae9d8bd32bf9f4bda1722b1
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-07-10 15:08:25 +0200

    Comment the code

--- client/src/service/dataservice.js
+++ client/src/service/dataservice.js
@@ -1,3 +1,6 @@
+// Category services
+
+// CREATE 
 function createCategory(name){
     const response = fetch("http://localhost:8000/category/create", {
         method: 'POST',
@@ -12,6 +15,39 @@ function createCategory(name){
     return response;
 }
 
+// READ
+function fetchCategories() {
+    const response = fetch("http://localhost:8000/category/all", {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    return response;
+};
+
+//DELETE
+
+function deleteCategory(name) {
+    const response = fetch("http://localhost:8000/category/delete", {
+        method: 'DELETE',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "name": name })
+    })
+    return response;
+};
+
+// Todo services
+
+// CREATE
 function createTodo(description, category, importance, letterhead) {
     const response = fetch("http://localhost:8000/todo/create", {
         method: 'POST',
@@ -30,61 +66,6 @@ function createTodo(description, category, importance, letterhead) {
     })
     return response;
 }
-
-function fetchTodos(category) {
-    const response = fetch("http://localhost:8000/todo/all", {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "category": category })
-        })
-        return response;
-};
-
-function fetchCategories() {
-    const response = fetch("http://localhost:8000/category/all", {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    return response;
-};
-
-function deleteTodo(description) {
-    const response = fetch("http://localhost:8000/todo/delete", {
-        method: 'DELETE',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "description": description })
-    })
-    return response;
-};
-
-function deleteCategory(name) {
-    const response = fetch("http://localhost:8000/category/delete", {
-        method: 'DELETE',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "name": name })
-    })
-    return response;
-};
 
 function todayTodos(category) {
     const response = fetch("http://localhost:8000/todo/today", {
@@ -110,6 +91,37 @@ function monthTodos(category) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ "category": category })
+    })
+    return response;
+};
+
+// READ
+function fetchTodos(category) {
+    const response = fetch("http://localhost:8000/todo/all", {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "category": category })
+        })
+        return response;
+};
+
+//DELETE
+
+function deleteTodo(description) {
+    const response = fetch("http://localhost:8000/todo/delete", {
+        method: 'DELETE',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "description": description })
     })
     return response;
 };
