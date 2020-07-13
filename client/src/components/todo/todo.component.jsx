0addf2237fef642b201f4e51adb1392886c1e741
NovakDetti <54432325+NovakDetti@users.noreply.github.com>
2020-07-13 13:51:55 +0200

    Improve todo component

--- client/src/components/todo/todo.component.jsx
+++ client/src/components/todo/todo.component.jsx
@@ -7,6 +7,8 @@ import './todo.style.scss';
 import { deleteTodo } from '../../service/dataservice';
 
 const TodoItem = ({details}) => {
+    const importanceBar = { level: details.importance}
+
     return(
         <div className="todo-container">
             <div id="informations" className={details.letterhead}>
@@ -15,9 +17,13 @@ const TodoItem = ({details}) => {
             </div>
             <h4>
                 {details.description}
+                <span class="material-icons build">build</span>
                 <span className="material-icons delete" onClick={() => deleteTodo(details.description)}>delete</span>
             </h4>
-            <p>Importance : {details.importance} %</p>
+            <div className="importance-container">
+                <p>Importance</p>
+                <div className="importance-bar" style={{ width: importanceBar.level/2+"vw" }}>{`${importanceBar.level}%`}</div>
+            </div>
         </div>
     )
 }
